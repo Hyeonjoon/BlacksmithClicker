9b49a9242ca4e78ff270d293f2bb3b5ebe9e1d35
Hyeonjoon <joon.lee.private@gmail.com>
2026-04-26 21:42:53 +0900

    Implement End of Game

--- Program.fs
+++ Program.fs
@@ -8,26 +8,31 @@ let main argv =
   let random = System.Random ()
   let rec gameLoop (state: GameState) = 
     displayState state
-    match getCommand () with
-    | "1" -> 
+    match getCommand (), state with
+    | _, s when s.CurrentSword.Level >= Constants.maxSwordLevel ->
+      "=============================" |> printfn "%s" 
+      "축하합니다. 100강 검을 획득했습니다." |> printfn "%s" 
+      "게임을 종료합니다." |> printfn "%s" 
+      0
+    | "1", _ -> 
       "=============================" |> printfn "%s" 
       let result = upgrade state random
       result.Message |> printfn "%s" 
       gameLoop result.State
-    | "2" ->
+    | "2", _ ->
       "=============================" |> printfn "%s" 
       let result = sell state
       result.Message |> printfn "%s" 
       gameLoop result.State
-    | "3" ->
+    | "3", _ ->
       "=============================" |> printfn "%s"
       "상태를 초기화합니다." |> printfn "%s"
       gameLoop StateGenerator.initialGameState
-    | "4" -> 
+    | "4", _ -> 
       "=============================" |> printfn "%s" 
       "게임을 종료합니다." |> printfn "%s" 
       0
-    | _ -> 
-      "Not Implemented" |> printfn "%s" 
+    | _, _ -> 
+      "지원하지 않는 옵션입니다." |> printfn "%s" 
       gameLoop state
   gameLoop StateGenerator.initialGameState
