1c5e0f48390f46618f2bd0ab1306dab75a97bd3a
Hyeonjoon <joon.lee.private@gmail.com>
2026-04-29 21:03:01 +0900

    Rearrange the UI to well-structured form

--- Program.fs
+++ Program.fs
@@ -6,38 +6,26 @@ open System
 [<EntryPoint>]
 let main argv =
   let random = System.Random ()
-  let rec gameLoop (state: GameState) = 
-    displayState state
+  let rec gameLoop (state: GameState) (lastMessage: string) = 
+    displayState state lastMessage
     match getCommand (), state with
     | _, s when s.CurrentSword.Level >= Constants.maxSwordLevel ->
-      "=============================" |> printfn "%s" 
-      "Congratulations. You get the Lv.100 legend sword." |> printfn "%s" 
-      "It's over!" |> printfn "%s" 
+      displayTerminationMessage "Congratulations. You get the Lv.100 legend sword. It's over!"
       0
     | "1", _ -> 
-      "=============================" |> printfn "%s" 
       let result = upgrade state random
-      result.Message |> printfn "%s" 
-      gameLoop result.State
+      gameLoop result.State result.Message
     | "2", _ ->
-      "=============================" |> printfn "%s" 
       let result = sell state
-      result.Message |> printfn "%s" 
-      gameLoop result.State
+      gameLoop result.State result.Message
     | "3", _ ->
-      "=============================" |> printfn "%s" 
       let result = purchasePermanently state
-      result.Message |> printfn "%s" 
-      gameLoop result.State
+      gameLoop result.State result.Message
     | "4", _ ->
-      "=============================" |> printfn "%s"
-      "The game has been initialized." |> printfn "%s"
-      gameLoop StateGenerator.initialGameState
-    | "5", _ -> 
-      "=============================" |> printfn "%s" 
-      "The game is being terminated." |> printfn "%s" 
+      gameLoop StateGenerator.initialGameState "The game has been initialized."
+    | "5", _ ->
+      displayTerminationMessage "The game is being terminated."
       0
     | _, _ -> 
-      "This is an unsupported option." |> printfn "%s" 
-      gameLoop state
-  gameLoop StateGenerator.initialGameState
+      gameLoop state "This is an unsupported option."
+  gameLoop StateGenerator.initialGameState "Welcome to Blacksmith Clicker!"
--- UI.fs
+++ UI.fs
@@ -2,13 +2,46 @@ module BlacksmithClicker.UI
 
 open BlacksmithClicker.State
 
-let displayState (state: GameState) =
-  printfn "-----------------------------"
-  printfn "Current Sword: %s (Lv.%d)" state.CurrentSword.Name state.CurrentSword.Level
-  printfn "Balance: %d Gold" state.Gold
-  printfn "Upgrade Cost: %d Gold | Selling Price: %d Gold | Upgrade Probability: %d%% | Permanent Purchase Cost: %d" state.CurrentSword.UpgradeCost state.CurrentSword.SellingPrice state.CurrentSword.UpgradeProbability state.CurrentSword.PermanentPurchaseCost
-  printfn "1. Upgrade Sword | 2. Sell Sword | 3. Purchase sword permanently | 4. Initialize Game | 5. Quit Game"
-  printf ">> "
+
+let private displayWidth = 85
+let private sectionDivider = String.replicate displayWidth "="
+let private innerDivider = String.replicate displayWidth "-"
+
+let private printRow (text: string) =
+  let innerWidth = displayWidth - 6 // Except '||(space)(space)||'
+  printfn "|| %-*s ||" innerWidth text
+
+let displayState (state: GameState) (lastMessage: string) =
+  // 1. Display NOTIFICATION section.
+  printfn "%s" sectionDivider
+  printRow (sprintf "!! NOTIFICATION: %s" lastMessage)
+
+  // 2. Display STATUS section.
+  printfn "%s" sectionDivider
+  printRow "!! STATUS"
+  printRow (sprintf "Current Sword: %s (Lv.%d)" state.CurrentSword.Name state.CurrentSword.Level)
+  printRow (sprintf "Balance: %d Gold" state.Gold)
+  
+  printfn "%s" innerDivider
+  let row1 = sprintf "Upgrade Cost: %d Gold  | Upgrade Probability: %d%%" state.CurrentSword.UpgradeCost state.CurrentSword.UpgradeProbability
+  let row2 = sprintf "Selling Price: %d Gold | Permanent Purchase Cost: %d" state.CurrentSword.SellingPrice state.CurrentSword.PermanentPurchaseCost
+  printRow row1
+  printRow row2
+
+  // 3. Display OPTIONS section.
+  printfn "%s" sectionDivider
+  printRow "!! OPTIONS"
+  printRow "1. Upgrade Sword | 2. Sell Sword | 3. Purchase sword permanently"
+  printRow "4. Initialize Game | 5. Quit Game"
+
+  // 4. Display guide line for option selection.
+  printfn "%s" innerDivider
+  printf ">> Select option: "
+
+let displayTerminationMessage (message: string) = 
+  printfn "%s" sectionDivider
+  printRow message
+  printfn "%s" sectionDivider
 
 let getCommand () =
   System.Console.ReadLine()
